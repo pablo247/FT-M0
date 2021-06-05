9e6d17118d52c0833a8993c29a16bce7b90a638a
Pablo De Jesús Moreno <pablo.24726@gmail.com>
2021-06-05 00:05:16 -0500

    Homework 03-JS-II JSII.test

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -5,20 +5,26 @@ function obtenerMayor(x, y) {
   // Devuelve el número más grande
   // Si son iguales, devuelve cualquiera de los dos
   // Tu código:
+  if (x === y) return x;
+  return (x > y) ? x : y;
 }
 
 function mayoriaDeEdad(edad) {
   //Determinar si la persona según su edad puede ingresar a un evento.
   //Si tiene 18 años ó más, devolver --> "Allowed"
   //Si es menor, devolver --> "Not allowed"
+  return (edad >= 18) ? "Allowed" : "Not allowed";
 }
-  
+
 function conection(status) {
-  //Recibimos un estado de conexión de un usuario representado por un valor numérico. 
+  //Recibimos un estado de conexión de un usuario representado por un valor numérico.
   //Cuando el estado es igual a 1, el usuario está "Online"
   //Cuando el estado es igual a 2, el usuario está "Away"
   //De lo contrario, presumimos que el usuario está "Offline"
   //Devolver el estado de conexión de usuario en cada uno de los casos.
+  if (status === 1) return "Online"
+  if (status === 2) return "Away"
+  return "Offline"
 }
 
 function saludo(idioma) {
@@ -28,16 +34,20 @@ function saludo(idioma) {
   // Si "idioma" es "ingles", devuelve "Hello!"
   // Si "idioma" no es ninguno de los anteiores o es `undefined` devuelve "Hola!"
   // Tu código:
+  if (idioma === "aleman") return "Guten Tag!"
+  if (idioma === "mandarin") return "Ni Hao!"
+  if (idioma === "ingles") return "Hello!"
+  return "Hola!"
 }
 
 /*
-Para implementar la siguiente función vamos a utilizar una estructura de control de flujo nueva, 
-llamada switch, si no estás familiarizado con esta estructura, es un buen escenario para hacerle 
-una visita al mejor amigo del programador, INTERNET! 
-En la carrera del desarrollador constantemente es necesario recurrir a los recursos que nos 
-proporciona la web (Google, YouTube, MDN, Stack Overflow, y muchos más) en búsqueda de las 
-herramientas necesarias para resolver los distintos problemas que nos enfrentamos, por lo que 
-aprender a realizar estas búsquedas en internet es uno de los skill más importantes que puedes 
+Para implementar la siguiente función vamos a utilizar una estructura de control de flujo nueva,
+llamada switch, si no estás familiarizado con esta estructura, es un buen escenario para hacerle
+una visita al mejor amigo del programador, INTERNET!
+En la carrera del desarrollador constantemente es necesario recurrir a los recursos que nos
+proporciona la web (Google, YouTube, MDN, Stack Overflow, y muchos más) en búsqueda de las
+herramientas necesarias para resolver los distintos problemas que nos enfrentamos, por lo que
+aprender a realizar estas búsquedas en internet es uno de los skill más importantes que puedes
 adquirir!
 En tu buscador basta con simplemente escribir "switch Javascript" y listo. Éxitos!
 */
@@ -49,18 +59,32 @@ function colors(color) {
   //En caso que el color recibido sea "orange", devuleve --> "This is orange"
   //Caso default: devuelve --> "Color not found"
   //Usar el statement Switch.
+  switch (color) {
+    case "blue":
+      return "This is blue";
+    case "red":
+      return "This is red";
+    case "green":
+      return "This is green";
+    case "orange":
+      return "This is orange";
+    default:
+      return "Color not found";
+  }
 }
 
 function esDiezOCinco(numero) {
   // Devuelve "true" si "numero" es 10 o 5
   // De lo contrario, devuelve "false"
   // Tu código:
+  return numero === 10 || numero === 5;
 }
 
 function estaEnRango(numero) {
   // Devuelve "true" si "numero" es menor que 50 y mayor que 20
   // De lo contrario, devuelve "false"
   // Tu código:
+  return numero < 50 && numero > 20;
 }
 
 function esEntero(numero) {
@@ -71,6 +95,7 @@ function esEntero(numero) {
   // De lo contrario, devuelve "false"
   // Pista: Puedes resolver esto usando `Math.floor`
   // Tu código:
+  return Math.floor(numero) === numero;
 }
 
 function fizzBuzz(numero) {
@@ -78,15 +103,29 @@ function fizzBuzz(numero) {
   // Si "numero" es divisible entre 5, devuelve "buzz"
   // Si "numero" es divisible entre 3 y 5 (ambos), devuelve "fizzbuzz"
   // De lo contrario, devuelve el numero
+  let message = "";
+  if (numero % 3 === 0) message += "fizz";
+  if (numero % 5 === 0) message += "buzz";
+  if (message.length === 0) return numero;
+  return message;
 }
 
 function operadoresLogicos(num1, num2, num3) {
-  //La función recibe tres números distintos. 
+  //La función recibe tres números distintos.
   //Si num1 es mayor a num2 y a num3 y además es positivo, retornar ---> "Número 1 es mayor y positivo"
   //Si alguno de los tres números es negativo, retornar ---> "Hay negativos"
   //Si num3 es más grande que num1 y num2, aumentar su valor en 1 y retornar el nuevo valor.
   //0 no es ni positivo ni negativo. Si alguno de los argumentos es 0, retornar "Error".
-  //Si no se cumplen ninguna de las condiciones anteriores, retornar false. 
+  //Si no se cumplen ninguna de las condiciones anteriores, retornar false.
+  if (num1 === 0 || num2 === 0 || num3 === 0)
+    return "Error";
+  if (num1 < 0 || num2 < 0 || num3 < 0)
+    return "Hay negativos";
+  if (num1 > num2 && num1 > num3)
+    return "Número 1 es mayor y positivo";
+  if (num3 > num1 && num3 > num2)
+    return ++num3;
+  return false;
 }
 
 function esPrimo(numero) {
@@ -95,32 +134,47 @@ function esPrimo(numero) {
   // Pista: un número primo solo es divisible por sí mismo y por 1
   // Pista 2: Puedes resolverlo usando un bucle `for`
   // Nota: Los números 0 y 1 NO son considerados números primos
+  if (numero === 0 || numero === 1) return false;
+  for (let index = 2; index < numero; index++) {
+    if (numero % index === 0) return false;
+  }
+  return true;
 }
 
 function esVerdadero(valor){
-  //Escribe una función que reciba un valor booleano y retorne “Soy verdadero” 
+  //Escribe una función que reciba un valor booleano y retorne “Soy verdadero”
   //si su valor es true y “Soy falso” si su valor es false.
   //Escribe tu código aquí
-
+  return valor ? "Soy verdadero" : "Soy falso";
 }
 
 function tablaDelSeis(){
   //Escribe una función que muestre la tabla de multiplicar del 6 (del 0 al 60).
   //La función devuelve un array con los resultados de la tabla de multiplicar del 6 en orden creciente.
-  //Escribe tu código aquí   
-  
+  //Escribe tu código aquí
+  let result = [];
+  for (let index = 0; index <= 10; index++) {
+    result.push(6*index);
+  }
+  return result;
 }
 
 function tieneTresDigitos(numero){
   //Leer un número entero y retornar true si tiene 3 dígitos. Caso contrario, retorna false.
   //Escribe tu código aquí
-  
+  return String(numero).length === 3;
 }
 
 function doWhile(numero) {
   //Implementar una función tal que vaya aumentando el valor recibido en 5 hasta un límite de 8 veces
   //Retornar el valor final.
   //Usar el bucle do ... while.
+  let count = 0
+  do {
+    numero += 5;
+    count++;
+  } while (count < 8);
+  return numero;
 }
 
 
